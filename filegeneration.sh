5ea5fc36f2c99b816944332f64a6e29ba1061dbf
rparajuliegen <rameshwor.parajuli@egen.ai>
2025-02-07 14:03:01 -0600

    Update filegeneration.sh
    added desc

--- filegeneration.sh
+++ filegeneration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ### This script createa an empty file with file name of current timestamp; 
 ### The file is then copied over to the GCS bucket
+### File creation/copy happens randomly anywhere between 1 to 10 seconds to introduce some variability to pubsub.
 ### Script runs until explicity killed.
 while true; do 
     sleep_time=$((RANDOM % 10 + 1))
